dfefbea76714048a7d93148f3b80c589c310e3cd
1357078925@qq.com <1357078925@qq.com>
2021-04-30 20:04:18 +0800

    master

---
+++ cat.py
@@ -0,0 +1,273 @@
+
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+from lr_utils import load_dataset
+#from public_tests import *
+
+# Loading the data (cat/non-cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+#train_set_x_orig ：保存的是训练集里面的图像数据（本训练集有209张64x64的图像（209，64，64，3））。
+#train_set_y_orig ：保存的是训练集的图像对应的分类值（【0 | 1】，0表示不是猫，1表示是猫）。
+#test_set_x_orig ：保存的是测试集里面的图像数据（本训练集有50张64x64的图像（50，64，64，3））。
+#test_set_y_orig ： 保存的是测试集的图像对应的分类值（【0 | 1】，0表示不是猫，1表示是猫）。
+#classes ： 保存的是以bytes类型保存的两个字符串数据，数据为：[b’non-cat’ b’cat’]。
+
+
+#预处理数据集
+#找出数据的尺寸和维度（m_train，m_test，num_px等）
+m_train=train_set_x_orig.shape[0]#训练集的数量209
+m_test=test_set_x_orig.shape[0]#测试集的数量50
+num_px=train_set_x_orig.shape[1]#每幅图片的分辨率60px*60px
+
+
+#重塑数据集，以使每个示例都是大小为（num_px * num_px * 3，1）的向量
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+#将维度为（a，b，c，d）的矩阵X展平为形状为(bcd, a)的矩阵
+
+#“标准化”数据
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+    s = 1 / (1 + np.exp(-z))
+    return s
+
+def initialize_with_zeros(dim):#初始化参数
+    """
+        此函数为w创建一个维度为（dim，1）的0向量，并将b初始化为0。
+        
+        参数：
+            dim  - 我们想要的w矢量的大小（或者这种情况下的参数数量）
+        
+        返回：
+            w  - 维度为（dim，1）的初始化向量。
+            b  - 初始化的标量（对应于偏差）
+    """
+    w = np.zeros((dim, 1))
+    b = 0
+
+    assert(w.shape == (dim, 1))#设置断言
+    assert(isinstance(b, float) or isinstance(b, int))#设置断言
+    
+    return w, b
+
+
+def propagate(w, b, X, Y):# 实现函数propagate（）来计算损失函数及其梯度。
+    '''
+    实现前向和后向传播的成本函数及其梯度。
+    参数：
+        w  - 权重，大小不等的数组（num_px * num_px * 3，1）
+        b  - 偏差，一个标量
+        X  - 矩阵类型为（num_px * num_px * 3，训练数量）
+        Y  - 真正的“标签”矢量（如果非猫则为0，如果是猫则为1），矩阵维度为(1,训练数据数量)
+
+    返回：
+        cost- 逻辑回归的负对数似然成本
+        dw  - 相对于w的损失梯度，因此与w相同的形状
+        db  - 相对于b的损失梯度，因此与b的形状相同
+    '''
+    m = X.shape[1]
+    
+    A = sigmoid(np.dot(w.T, X) + b)            # compute activation
+    cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))         # compute cost
+
+    
+    dw = 1 / m * np.dot(X, (A - Y).T)
+    db = 1 / m * np.sum(A - Y)
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):#写下优化函数。 目标是通过最小化损失函数J来学习 w 和 b 。
+    '''
+    参数：
+        w  - 权重，大小不等的数组（num_px * num_px * 3，1）
+        b  - 偏差，一个标量
+        X  - 维度为（num_px * num_px * 3，训练数据的数量）的数组。
+        Y  - 真正的“标签”矢量（如果非猫则为0，如果是猫则为1），矩阵维度为(1,训练数据的数量)
+        num_iterations  - 优化循环的迭代次数
+        learning_rate  - 梯度下降更新规则的学习率
+        print_cost  - 每100步打印一次损失值
+    
+    返回：
+        params  - 包含权重w和偏差b的字典
+        grads  - 包含权重和偏差相对于成本函数的梯度的字典
+        costs - 优化期间计算的所有成本列表，将用于绘制学习曲线。
+    '''
+    costs = []
+    
+    for i in range(num_iterations):
+        
+        
+        grads, cost = propagate(w, b, X, Y)
+        
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        
+        w = w - learning_rate * dw
+        b = b - learning_rate * db
+
+        '''
+        if i % 100 == 0:#记录成本
+            costs.append(cost)
+        
+        if print_cost and i % 100 == 0:#打印成本数据
+            print("迭代的次数: %i ， 误差值： %f" % (i,cost))
+        '''
+    
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
+
+
+def predict(w, b, X):
+    '''
+    使用学习逻辑回归参数logistic （w，b）预测标签是0还是1，
+    
+    参数：
+        w  - 权重，大小不等的数组（num_px * num_px * 3，1）
+        b  - 偏差，一个标量
+        X  - 维度为（num_px * num_px * 3，训练数据的数量）的数据
+    
+    返回：
+        Y_prediction  - 包含X中所有图片的所有预测【0 | 1】的一个numpy数组（向量）
+    '''
+    
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+    
+    
+    A = sigmoid(np.dot(w.T , X) + b)
+    
+    for i in range(A.shape[1]):
+        
+        
+        if A[0, i] > 0.5 :
+            Y_prediction[0,i] = 1
+        else:
+            Y_prediction[0,i] = 0
+        
+    
+    return Y_prediction
+
+#将所有功能合并到模型中
+def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
+    """
+    通过调用之前实现的函数来构建逻辑回归模型
+    
+    参数：
+        X_train  - numpy的数组,维度为（num_px * num_px * 3，m_train）的训练集
+        Y_train  - numpy的数组,维度为（1，m_train）（矢量）的训练标签集
+        X_test   - numpy的数组,维度为（num_px * num_px * 3，m_test）的测试集
+        Y_test   - numpy的数组,维度为（1，m_test）的（向量）的测试标签集
+        num_iterations  - 表示用于优化参数的迭代次数的超参数
+        learning_rate  - 表示optimize（）更新规则中使用的学习速率的超参数
+        print_cost  - 设置为true以每100次迭代打印成本
+    
+    返回：
+        d  - 包含有关模型信息的字典。
+    """
+    
+    w, b = initialize_with_zeros(X_train.shape[0])
+    
+    params, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    
+    w = params["w"]
+    b = params["b"]
+    
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+    
+    '''
+    if print_cost:
+        print("训练集的准确性: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+        print("测试集的准确性: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+    '''
+    
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    
+    return d
+
+logistic_regression_model = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=2000, learning_rate=0.005, print_cost=True)
+'''
+#绘制损失图像
+costs = np.squeeze(logistic_regression_model['costs'])
+print(costs)
+plt.plot(costs)
+plt.ylabel('cost')
+plt.xlabel('iterations (per hundreds)')
+plt.title("Learning rate =" + str(logistic_regression_model["learning_rate"]))
+plt.show()
+'''
+
+'''
+#不同学习速率下，损失的图像
+learning_rates = [0.01, 0.001, 0.0001]
+models = {}
+
+for lr in learning_rates:
+    print ("Training a model with learning rate: " + str(lr))
+    models[str(lr)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=1500, learning_rate=lr, print_cost=False)
+    print ('\n' + "-------------------------------------------------------" + '\n')
+
+for lr in learning_rates:
+    plt.plot(np.squeeze(models[str(lr)]["costs"]), label=str(models[str(lr)]["learning_rate"]))
+
+plt.ylabel('cost')
+plt.xlabel('iterations (hundreds)')
+
+legend = plt.legend(loc='upper center', shadow=True)
+frame = legend.get_frame()
+frame.set_facecolor('0.90')
+plt.show()
+'''
+
+
+
+#-使用自己的图像进行测试
+# change this to the name of your image file
+my_image = "my_image.jpg"   
+
+# We preprocess the image to fit your algorithm.
+fname = "images/" + my_image
+image = np.array(Image.open(fname).resize((num_px, num_px)))
+plt.imshow(image)
+image = image / 255.
+image = image.reshape((1, num_px * num_px * 3)).T
+my_predicted_image = predict(logistic_regression_model["w"], logistic_regression_model["b"], image)
+
+print("y = " + str(np.squeeze(my_predicted_image)) + ", your algorithm predicts a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
